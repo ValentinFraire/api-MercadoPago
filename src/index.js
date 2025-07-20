3d78c3fd50c7d9a4ef650277db933340845df204
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2025-07-19 23:07:04 -0600

    pagina inicio

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import paymentRoutes from './routes/payment.routes.js';
-import morgan from 'morgan';
-// importar cors 
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import paymentRoutes from "./routes/payment.routes.js";
+import morgan from "morgan";
+// importar cors
+import cors from "cors";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors(
-  {
-    origin: process.env.BASE_URL || '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }
-)); 
-app.use(morgan('dev'),);
+app.use(
+  cors({
+    origin: process.env.BASE_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+app.use(morgan("dev"));
 app.use(express.json());
-app.use('/api/payments', paymentRoutes);
+app.use("/api/payments", paymentRoutes);
+
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "Bienvenido a la API de MercadoPago" });
+});
 
 app.use((req, res, next) => {
-  res.status(404).json({ error: 'Ruta no encontrada' });
+  res.status(404).json({ error: "Ruta no encontrada" });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Error interno del servidor' });
+  res.status(500).json({ error: "Error interno del servidor" });
 });
 
 app.listen(port, () => {
